2d7c5c1d99fde3872e37060e92259c9619672784
Logan <logandavistx@gmail.com>
2026-01-09 17:42:44 -0600

    Added code as per the core requirements. Ask user for grade percentage, display message for passing or not, changed code to use letter variable.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,43 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+
+        Console.Write("Enter your grade percentage: ");
+        string gradePercentage = Console.ReadLine();
+        int percentage = int.Parse(gradePercentage);
+        
+        string letter = "";
+
+        if (percentage >= 90)
+        {
+            letter = "A";
+        }
+        else if (percentage >= 80)
+        {
+            letter = "B";
+        }
+        else if (percentage >= 70)
+        {
+            letter = "C";
+        }
+        else if (percentage >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}");
+        
+        if (percentage >= 70)
+        {
+            Console.WriteLine("Congratulations! You've passed!");
+        }
+        else
+        {
+            Console.WriteLine("Unfortunately, you didn't pass, but you'll do better next time!");
+        }
     }
 }
