2ca05787a4e554a0bfd0e693817b05d9118e6957
Logan <logandavistx@gmail.com>
2026-01-09 19:41:17 -0600

    Removed "Hello Prep4 World!" Added core requirement code to prompt for numbers, compute the sum, the average, and maximum in the list.

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+
+        int numberValue = -1;
+        while (numberValue != 0)
+        {
+            Console.Write("Enter a list of numbers, type 0 when you're finished.");
+
+            string userInput = Console.ReadLine();
+            numberValue = int.Parse(userInput);
+
+            if (numberValue != 0)
+            {
+                numbers.Add(numberValue);
+            }
+        }
+
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+
+        Console.WriteLine($"The sum is: {sum}");
+
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        int max = numbers[0];
+
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+
+        Console.WriteLine($"The max is: {max}");
     }
 }
