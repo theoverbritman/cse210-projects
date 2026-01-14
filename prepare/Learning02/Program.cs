fe3737098009cf1f69bc9d7903258731cf6c9bee
Logan <logandavistx@gmail.com>
2026-01-14 13:53:57 -0600

    Added week 2 prepare abstraction code files.

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "Microsoft";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = "2020";
+        job1._endYear = "2025";
+
+    
+        Job job2 = new Job();
+        job1._company = "Apple";
+        job1._jobTitle = "Administrator";
+        job1._startYear = "2018";
+        job1._endYear = "2023";
+
+        Resume myResume = new Resume();
+        myResume._name = "Logan Davis";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
