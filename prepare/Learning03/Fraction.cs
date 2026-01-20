494c06710fc02c7c7f9b67a96cb81b40f48e20f3
Logan <logandavistx@gmail.com>
2026-01-20 17:54:16 -0600

    Added week 3 learning activity code

---
+++ prepare/Learning03/Fraction.cs
@@ -0,0 +1,65 @@
+using System;
+
+
+public class Fraction
+{
+    // Private attributes
+    private int _top;
+    private int _bottom;
+
+    // Constructor
+    public Fraction()
+    {
+        _top = 1;
+        _bottom = 1;
+    }
+
+    public Fraction(int wholeNumber)
+    {
+        _top = wholeNumber;
+        _bottom = 1;
+    }
+
+    public Fraction(int top, int bottom)
+    {
+        _top = top;
+        SetBottom(bottom);
+    }
+
+    public void SetBottom(int bottom)
+    {
+        if (bottom != 0)
+        {
+            _bottom = bottom;
+        }
+        else
+        {
+            _bottom = 1;
+        }
+    }
+
+    public void SetTop(int top)
+    {
+        _top = top;
+    }
+
+    public int GetTop()
+    {
+        return _top;
+    }
+    public int GetBottom()
+    {
+        return _bottom;
+    }
+
+    public string GetFractionString()
+    {
+        string text = $"{_top}/{_bottom}";
+        return text;
+    }
+
+    public double GetDecimalValue()
+    {
+        return (double)_top / (double)_bottom;
+    }
+}
