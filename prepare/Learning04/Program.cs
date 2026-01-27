e4c8257d82d28e4b8e88e7573fc38fb369641ad1
Logan <logandavistx@gmail.com>
2026-01-27 15:14:08 -0600

    Added week 4 learning activity code.

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assignment1 = new Assignment("Logan Davis", "Algebra");
+        Console.WriteLine(assignment1.GetSummary());
+        
+        MathAssignment assignment2 = new MathAssignment("Jeremy Fraiser", "Fractions", "4.2", "3-14");
+        Console.WriteLine(assignment2.GetSummary());
+        Console.WriteLine(assignment2.GetHomeworkList());
+
+        WritingAssignment assignment3 = new WritingAssignment("Richard Smith", "American Government", "What is Lobbying?");
+        Console.WriteLine(assignment3.GetSummary());
+        Console.WriteLine(assignment3.GetWritingInformation());
     }
 }
