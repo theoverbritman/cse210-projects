0fb23c0ed03697056afec4e77a876df84c65ff47
Logan <logandavistx@gmail.com>
2026-02-03 13:12:09 -0600

    Added week 5 learning activity code files.

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square sh1 = new Square("Red", 3);
+        shapes.Add(sh1);
+
+        Rectangle sh2 = new Rectangle("Blue", 4, 5);
+        shapes.Add(sh2);
+
+        Circle sh3 = new Circle("Green", 6);
+        shapes.Add(sh3);
+
+        foreach (Shape sh in shapes)
+        {
+            string color = sh.GetColor();
+
+            double area = sh.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
     }
 }
